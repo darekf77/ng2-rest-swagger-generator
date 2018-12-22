677e931f37814b69839c792ee888d414995ec7db
Dariusz Filipiak <darekf77@gmail.com>
2018-12-22 20:10:50 +0100

    in porgress of new version

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,125 @@
-var webpack = require('webpack'),
-    path = require('path'),
-    fs = require('fs'),
-    WebpackStrip = require('strip-loader');
+/**
+ * webpack.config.js
+ *
+ * This config serves as both the development and production
+ * Webpack config. The difference is that it's consumed by
+ * either webpack-dev-server (development) or webpack itself
+ * (production)
+ */
 
-var WebpackOnBuildPlugin = require('on-build-webpack');
+const webpack = require('webpack');
+const path = require('path');
+const fse = require('fs-extra');
+const fs = require('fs');
+const _ = require('lodash')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyWebpackPlugin = require('uglifyjs-webpack-plugin');
+const { fixWebpackEnv } = require('tnp-bundle')
+const DefinePlugin = require('webpack/lib/DefinePlugin');
 
-var nodeModules = {};
-fs.readdirSync('node_modules')
-    .filter(function (x) {
-        return ['.bin'].indexOf(x) === -1;
-    })
-    .forEach(function (mod) {
-        nodeModules[mod] = 'commonjs ' + mod;
-    });
+const noEmitOnErrorsPlugin = new webpack.NoEmitOnErrorsPlugin();
 
-module.exports = {
-    entry: './src/generator.ts',
-    target: 'node',
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+  title: 'Sample TypeScript App',
+  inject: true
+});
+
+const uglifyWebpackPlugin = new UglifyWebpackPlugin();
+
+
+
+
+/**
+ * Export config
+ */
+module.exports = (env) => {
+  env = env || {};
+  fixWebpackEnv(env)
+  const { watch = false, outFolder = 'dist', moduleName = undefined, port = 9000 } = env;
+  // console.log('env', env)
+  const browserOutFolder = `browser-for-${moduleName}`;
+  const srcDirRelative = _.isString(moduleName) ? `./tmp-src-${outFolder}-${browserOutFolder}` : `./tmp-src-${outFolder}-browser`;
+  const distDirRelative = _.isString(moduleName) ? `./${browserOutFolder}` : `./${outFolder}-browser`;
+
+  const srcDir = path.join(__dirname, srcDirRelative);
+  const distDir = path.join(__dirname, distDirRelative);
+
+  let ENV = moduleName ? fse.readJSONSync('./tmp-environment.json', {
+    'encoding': 'utf8'
+  }) : {}
+
+
+  return {
+    devtool: !watch ? 'source-map' : 'eval-source-map',
+    entry: `${srcDirRelative}/app.ts`,
     output: {
-        // path: __dirname + '/bin',
-        libraryTarget: "commonjs",
-        filename: 'bundle.js'
+      path: distDir,
+      filename: '[name].[hash:5].js'
     },
     resolve: {
-        extensions: ['', '.ts', '.js']
+      extensions: ['.ts', '.tsx', '.js']
     },
     module: {
-        loaders: [
-            { test: /\.ts$/, loaders: ['ts-loader'] },
-            { test: /\.json$/, loaders: ['json-loader'] }
-        ]
-    },
-    externals: nodeModules,
-    node: {
-        fs: "empty",
-        __dirname: false,
-        __filename: false
+      rules: [
+        // {
+        //   enforce: 'pre',
+        //   test: /\.tsx?$/,
+        //   loader: 'tslint-loader',
+        //   include: srcDir,
+        //   exclude: /node_modules/,
+        //   options: {
+        //     configFile: './tslint.json',
+        //     failOnHint: true
+        //   }
+        // },
+        {
+          test: /\.tsx?$/,
+          include: srcDir,
+          exclude: /node_modules/,
+          use: [
+            {
+              /**
+               * 2. Transpile ES6 + dynamic imports into ES5
+               *    (smaller bundle sizes than ts-loader alone)
+               */
+              loader: 'babel-loader',
+              options: {
+                presets: ['es2015'],
+                plugins: ['babel-plugin-syntax-dynamic-import']
+              }
+            },
+            {
+              /**
+               * 1. Transpile TypeScript into ES6 + dynamic imports
+               */
+              loader: 'ts-loader',
+              options: {
+                // compilerOptions: {
+                // module: 'esnext' , // allows bundle splitting via dynamic imports!,
+                // },
+                configFile: "tmp-src-dist-browser/tsconfig.json"
+              }
+            }
+          ]
+        }
+      ]
     },
     plugins: [
-        // new webpack.optimize.UglifyJsPlugin({
-        //     compress: {
-        //         warnings: false
-        //     }
-        // }),
-        new WebpackOnBuildPlugin(function (stats) {
-            // Do whatever you want... 
-        }),
-    ]
+      new DefinePlugin({
+        "ENV": JSON.stringify(ENV)
+      }),
+      noEmitOnErrorsPlugin,
+      htmlWebpackPlugin,
+      ...(!watch
+        ? [uglifyWebpackPlugin]
+        : [])
+    ],
+    devServer: {
+      contentBase: srcDir,
+      compress: true,
+      port
+    }
+  };
 }
+
 
