e3ed7fb804b515a778c7eca79a773b31f811fce8
Dariusz Filipiak <dariusz.filipiak@igt.com>
2022-10-01 17:35:01 +0200

    new version 6.0.26

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+//#region @notForNpm
+
+//#region @browser
+    import { NgModule } from '@angular/core';
+    import { Component, OnInit } from '@angular/core';
+
+
+    @Component({
+      selector: 'app-ng2-rest-swagger-generator',
+      template: 'hello from ng2-rest-swagger-generator'
+    })
+    export class Ng2RestSwaggerGeneratorComponent implements OnInit {
+      constructor() { }
+
+      ngOnInit() { }
+    }
+
+    @NgModule({
+      imports: [],
+      exports: [Ng2RestSwaggerGeneratorComponent],
+      declarations: [Ng2RestSwaggerGeneratorComponent],
+      providers: [],
+    })
+    export class Ng2RestSwaggerGeneratorModule { }
+    //#endregion
+
+    //#region @backend
+    async function start(port: number) {
+      console.log('hello world from backend');
+    }
+
+    export default start;
+
+//#endregion
+
+//#endregion
---
+++ src/lib/index.ts
@@ -0,0 +1,2 @@
+export function helloWorldFromNg2RestSwaggerGenerator() { }
+        
