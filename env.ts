a11118427e49cd45ed93df12bfebb1f3a81d2f86
Dariusz Filipiak <darekf77@gmail.com>
2025-04-08 20:26:31 +0200

    release: v18.0.18

---
+++ env.ts
@@ -0,0 +1,9 @@
+import type { EnvOptions } from 'tnp/src';
+
+const env: Partial<EnvOptions> = {
+  website: {
+    domain: 'ng2-rest-swagger-generator.example.domain.com',
+    title: 'Ng 2 Rest Swagger Generator',
+  },
+};
+export default env;
